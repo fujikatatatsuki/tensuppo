4d4ec685b883065b7cf03a76e425f0505cd04744
fujikatatatsuki <t1t2.40001@gmail.com>
2020-10-09 14:18:39 +0900

    テスト用ダミーコードの修正

--- spec/factories/monthly_payments.rb
+++ spec/factories/monthly_payments.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     phone_cost                       { 10000 }  # 携帯料金
     other_communication_costs        { 10000 }  # その他通信費
     scholarship                      { 10000 }  # 奨学金
-    debt                             { 10000 }  # 借金
     other_monthly_payment            { 0 }      # その他
   end
 end
